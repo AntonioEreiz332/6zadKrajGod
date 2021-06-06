0ff79ffa6c083b7abf8e9fa9dfd0b7605a85e33d
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-06-06 16:53:47 +0200

    Add files via upload

---
+++ 6Ponavljanje/6Ponavljanje/KlasaC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6Ponavljanje
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] Broj)
+        {
+            int Zbroj = Broj.Sum();
+            return Zbroj;
+        }
+        public double Prosjek(int[] Broj)
+        {
+            double Prosjek = Broj.Average();
+            return Prosjek;
+        }
+
+
+            
+
+    }
+}
---
+++ 6Ponavljanje/6Ponavljanje/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6Ponavljanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nizBrojeva = new int[5];
+            int zbroj;
+            double prosjek;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Upisite " + (i + 1) + ". broj: ");
+                nizBrojeva[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            KlasaC newObjekt = new KlasaC();
+            zbroj = newObjekt.Zbroj(nizBrojeva);
+            prosjek = newObjekt.Prosjek(nizBrojeva);
+            Console.WriteLine("Zbroj iznosi: " + zbroj + "\n" + "Prosjek iznosi: " + prosjek);
+
+
+            Console.ReadKey();
+        }
+    }
+}
